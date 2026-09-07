5bd3b7dccbc8e160ab2318567dfc9ba192d4b228
PH536-UI <Paulohenrique2020pereira@gmail.com>
2026-09-07 17:28:37 +0000

    feat: P0.2 final - 3 buckets OAC (primary+dr+app_assets) - CDN 200 OK - Game Day ready

--- terraform/p0_2_s3_oac_policy.tf
+++ terraform/p0_2_s3_oac_policy.tf
@@ -13,7 +13,6 @@ data "aws_iam_policy_document" "primary_oac" {
     }
   }
 }
-
 data "aws_iam_policy_document" "dr_oac" {
   statement {
     actions   = ["s3:GetObject"]
@@ -29,15 +28,33 @@ data "aws_iam_policy_document" "dr_oac" {
     }
   }
 }
-
+data "aws_iam_policy_document" "app_assets_oac" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.app_assets.arn}/*"]
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.cdn.arn, aws_cloudfront_distribution.app[0].arn]
+    }
+  }
+}
 resource "aws_s3_bucket_policy" "primary_oac" {
   provider = aws.us_east_1
   bucket   = aws_s3_bucket.primary.id
   policy   = data.aws_iam_policy_document.primary_oac.json
 }
-
 resource "aws_s3_bucket_policy" "dr_oac" {
   provider = aws.us_east_1
   bucket   = aws_s3_bucket.dr.id
   policy   = data.aws_iam_policy_document.dr_oac.json
+}
+resource "aws_s3_bucket_policy" "app_assets_oac" {
+  provider = aws.us_east_1
+  bucket   = aws_s3_bucket.app_assets.id
+  policy   = data.aws_iam_policy_document.app_assets_oac.json
 }
