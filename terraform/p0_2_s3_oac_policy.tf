231ead1d011f4c2d8602d0e697d8d1115c0c28d7
PH536-UI <Paulohenrique2020pereira@gmail.com>
2026-09-07 17:18:03 +0000

    fix: P0.2 OAC policy validada (CloudFront -> S3 404->200) + pitch final OK - Time02 ready

---
+++ terraform/p0_2_s3_oac_policy.tf
@@ -0,0 +1,43 @@
+data "aws_iam_policy_document" "primary_oac" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.primary.arn}/*"]
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.cdn.arn, aws_cloudfront_distribution.app[0].arn]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "dr_oac" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.dr.arn}/*"]
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.cdn.arn, aws_cloudfront_distribution.app[0].arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "primary_oac" {
+  provider = aws.us_east_1
+  bucket   = aws_s3_bucket.primary.id
+  policy   = data.aws_iam_policy_document.primary_oac.json
+}
+
+resource "aws_s3_bucket_policy" "dr_oac" {
+  provider = aws.us_east_1
+  bucket   = aws_s3_bucket.dr.id
+  policy   = data.aws_iam_policy_document.dr_oac.json
+}
