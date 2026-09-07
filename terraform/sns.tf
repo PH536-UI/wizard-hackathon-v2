499814e7fbb438cb2643c3c36db7347c9177fa9c
PH536-UI <Paulohenrique2020pereira@gmail.com>
2026-09-07 16:40:48 +0000

    feat: SNS 5 emails confirmados (Thamara+Vagner) + WAF 403 bloqueado validado - Game Day ready 08/09

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -1,14 +1,17 @@
-resource "aws_sns_topic" "budget_alerts" {
-  name = "wizard-budget-alerts"
-  tags = {
-    Project = "wizard"
-    Event   = "WizardCloudHackathon2026"
-    Team    = "Time-02"
-  }
+resource "aws_sns_topic" "alerts" {
+  name = "wizard-team-security-alerts"
 }
 
-resource "aws_sns_topic_subscription" "budget_email" {
-  topic_arn = aws_sns_topic.budget_alerts.arn
+resource "aws_sns_topic_subscription" "email_alerts" {
+  for_each = toset([
+    "paulohenrique2020pereira@gmail.com",
+    "priscilla.correa151091@gmail.com",
+    "rndo.negocio@gmail.com",
+    "thamara_catharine@hotmail.com",
+    "vagnertomaz@hotmail.com"
+  ])
+
+  topic_arn = aws_sns_topic.alerts.arn
   protocol  = "email"
-  endpoint  = "team02@example.com"
+  endpoint  = each.value
 }
